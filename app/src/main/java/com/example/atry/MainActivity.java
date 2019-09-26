00993a1fda3b9b33dc4b6b9de0e682573b0ed2e0
Boyoung Song <boyoungsong@Boyoungs-MacBook-Pro.local>
2019-09-26 17:11:23 -0400

    First Class

--- app/src/main/java/com/example/atry/MainActivity.java
+++ app/src/main/java/com/example/atry/MainActivity.java
@@ -2,20 +2,63 @@ package com.example.atry;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-    public void openNumbersList(View view){
-        Intent i = new Intent(this, NumbersActivity.class);
-        startActivity(i);
-    }
 
+        //Set the content of the activity to use the activity_main.xml layut file
+        setContentView(R.layout.activity_main);
+
+        //Find the View that shows the numbers category
+        TextView numbers = (TextView)findViewById(R.id.numbers);
+        //Set a clicklistener on that View
+        numbers.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                //Create a new intent to open the {@link NumbersActivity}
+                Intent numbersIntent = new Intent(MainActivity.this,NumbersActivity.class);
+                //Start the new activity
+                startActivity(numbersIntent);
+            }
+        });
+
+        //Colors
+        TextView colors = (TextView)findViewById(R.id.colors);
+        colors.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent colorsIntent = new Intent(MainActivity.this, ColorsActivity.class);
+                startActivity(colorsIntent);
+            }
+        });
+
+        // Family
+        TextView family = (TextView)findViewById(R.id.family);
+        family.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent familyIntent = new Intent(MainActivity.this, FamilyActivity.class);
+                startActivity(familyIntent);
+            }
+        });
+
+        // Pharases
+        TextView phrases = (TextView)findViewById(R.id.phrases);
+        phrases.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent phrasesIntent = new Intent(MainActivity.this, PhrasesActivity.class);
+                startActivity(phrasesIntent);
+            }
+        });
+
+    }
 }
