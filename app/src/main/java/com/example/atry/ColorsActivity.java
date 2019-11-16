cd919b8fd884c33a704d82f67a9ca9f89d8d88ec
BoYoung Song <boyoung_song@hotmail.com>
2019-11-16 16:01:20 -0500

    Lesson 3 Complete

--- app/src/main/java/com/example/atry/ColorsActivity.java
+++ app/src/main/java/com/example/atry/ColorsActivity.java
@@ -3,6 +3,9 @@ package com.example.atry;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class ColorsActivity extends AppCompatActivity {
 
@@ -10,5 +13,39 @@ public class ColorsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_colors);
+
+
+        //Create an array of words
+        ArrayList<Word> words = new ArrayList<Word>();
+        words.add(new Word("red","wetetti", R.drawable.color_red));
+        words.add(new Word("mustard yellow","chiwiita", R.drawable.color_mustard_yellow));
+        words.add(new Word("dusty yellow","topiise", R.drawable.color_dusty_yellow));
+        words.add(new Word("green","chokokki", R.drawable.color_green));
+        words.add(new Word("brown","takaakki", R.drawable.color_brown));
+        words.add(new Word("gray","topoppi", R.drawable.color_gray));
+        words.add(new Word("black","kululli", R.drawable.color_black));
+        words.add(new Word("white","kelelli", R.drawable.color_white));
+
+
+
+
+        // Create an {@link ArrayAdapter}, whose data source is a list of Strings. The
+        // adapter knows how to create layouts for each item in the list, using the
+        // simple_list_item_1.xml layout resource defined in the Android framework.
+        // This list item layout contains a single {@link TextView}, which the adapter will set to
+        // display a single word.
+        WordAdapter adapter =
+                new WordAdapter(this, words,R.color.category_colors);
+
+        // Find the {@link ListView} object in the view hierarchy of the {@link Activity}.
+        // There should be a {@link ListView} with the view ID called list, which is declared in the
+        // activity_numbers.xml layout file.
+        ListView listView = (ListView) findViewById(R.id.list);
+
+        // Make the {@link ListView} use the {@link ArrayAdapter} we created above, so that the
+        // {@link ListView} will display list items for each word in the list of words.
+        // Do this by calling the setAdapter method on the {@link ListView} object and pass in
+        // 1 argument, which is the {@link ArrayAdapter} with the variable name itemsAdapter.
+        listView.setAdapter(adapter);
     }
 }
--- app/src/main/java/com/example/atry/WordAdapter.java
+++ app/src/main/java/com/example/atry/WordAdapter.java
@@ -5,17 +5,21 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
 
 import java.util.ArrayList;
 
 public class WordAdapter extends ArrayAdapter<Word> {
+    private int mColorResourceId;
 
-    public WordAdapter(Activity context, ArrayList<Word> words){
+    public WordAdapter(Activity context, ArrayList<Word> words,int colorResourceId){
         super(context,0,words);
+        mColorResourceId = colorResourceId;
 
     }
 
@@ -44,6 +48,28 @@ public class WordAdapter extends ArrayAdapter<Word> {
         // Get the version number from the current AndroidFlavor object and
         // set this text on the number TextView
         defaultTextView.setText(currentWord.getDefaultTranslation());
+
+        //Find the ImageView in the list_item.xml layout with the ID image.
+        ImageView imageView = (ImageView) listItemView.findViewById(R.id.image);
+
+        if(currentWord.hasImage()){
+            // set the ImageView to the image resource specified in the current Word
+            imageView.setImageResource(currentWord.getImageResourceId());
+
+            //make sure the image is visible
+            imageView.setVisibility(View.VISIBLE);
+        }
+        else{
+            //Otherwise hide the Imageview
+            imageView.setVisibility(View.GONE);
+        }
+
+        //Set the theme color for the list item
+        View textContainer = listItemView.findViewById(R.id.text_container);
+        //Find the color that the resource ID maps to
+        int color = ContextCompat.getColor(getContext(),mColorResourceId);
+        //Set the background color of the text container View
+        textContainer.setBackgroundColor(color);
 
         // Return the whole list item layout (containing 2 TextViews and an ImageView)
         // so that it can be shown in the ListView
